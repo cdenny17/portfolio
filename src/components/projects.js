67a62cd23c802f34eafe25453d656d10985a31e0
Cdenny <cdenny83@gmail.com>
2020-04-10 18:39:45 -0700

    copy edits

--- src/components/projects.js
+++ src/components/projects.js
@@ -20,9 +20,9 @@ export default function Projects(props) {
                             alt='Boundless WebVR landing page'
                         />
                         <div className='card-body'>
-                            <p><a href='https://christiandenny.com/webvr'>Check out My Web-based VR Project</a></p>
-                            <p>Using Webpack, The WebXR API, WebGL API and THREE.js I built a web-based Virtual Reality experience.</p>
-
+                            <h3>Boundless WebVR</h3>
+                            <p>Using Webpack, The WebXR API, WebGL API and THREE.js I built a web-based Virtual Reality experience. Connect an HTC Vive to play an archery game without having to download it.</p>
+                            <button type='button' className='btn btn-outline-primary'><a href='https://christiandenny.com/webvr'>Check out Boundless WebVR</a></button>
                         </div>
                     </div>
 
@@ -37,9 +37,10 @@ export default function Projects(props) {
                             muted
                         />
                         <div className='card-body'>
-                            <p><a href='https://christiandenny.com/revolt-coffee'>Check out Revolt Coffee AR</a></p>
-                            <p>This is a Web-based augmented reality campagne for the Revolt Coffee company.
-                            As sole developer on this project I collaborated with designer and content creator to create a unique add campagne for The Corwin Bevearge Company, the owners of the Revolt Coffee brand.</p>
+                            <h3>Revolt Coffee AR</h3>
+                            <p>This is a Web-based augmented reality site for Revolt Coffee.
+                            As sole developer on this project I collaborated with a designer and animator to create a unique add campaign.</p>
+                            <button type='button' className='btn btn-outline-primary'><a href='https://christiandenny.com/revolt-coffee'>Check out Revolt Coffee AR</a></button>
                         </div>
                     </div>
 
@@ -52,10 +53,12 @@ export default function Projects(props) {
                             alt='VanParksVision Mobile APP landing page'
                         />
                         <div className='card-body'>
-                            <p><a href='https://dtc-wsuv.org/projects/vanparksvision/index.html'>Check out VanParksVision Mobile App</a></p>
-                            <p>As a developer for the VanParksVision Mobile App, I used react native to build an interactive Augmented Reality experience.
+                            <h3>VanParksVision Mobile App</h3>
+                            <p>As a developer for the VanParksVision Mobile App, using React-Native I built an interactive Augmented Reality experience.
                             Additionally, I deployed the mobile app to both Apple and Android store within the deadline.
                             </p>
+                            <button type='button' className='btn btn-outline-primary'><a href='https://dtc-wsuv.org/projects/vanparksvision/index.html'>Check out VanParksVision</a></button>
+
 
                         </div>
                     </div>
@@ -73,8 +76,9 @@ export default function Projects(props) {
                             muted
                         />
                         <div className='card-body'>
-                            <p><a href='https://dtc-wsuv.org/cdenny17/message/'> Check out The Message </a></p>
+                            <h3>The Message</h3>
                             <p>Using Three.js I rendered a cube in a 3D space that plays Marshall McLuhan's video on "The Media is the Massage". It was a fun project to test out video capabilities in Three.js.</p>
+                            <button type='button' className='btn btn-outline-primary'><a href='https://dtc-wsuv.org/cdenny17/message/'> Check out The Message </a></button>
                         </div>
                     </div>
 
@@ -85,10 +89,10 @@ export default function Projects(props) {
                             alt='Chore App landing page'
                         />
                         <div className='card-body'>
-                            <p><a href='https://christiandenny.com/chore-app'>Check out Chore Time</a></p>
-                            <p>This chore app was made using React.js, it utilizes local storage to keep the list of people and their asigned
-                            chore active until removed. Also, a color is randomly asigned to each name entered to keep the list of chores organized.
-                                You can enter a few prizes, and once all the chores are checked off tap the spinning wheel and one prize is randomly selected.</p>
+                            <h3>Chore App</h3>
+                            <p>Chore App was made using React.js, it utilizes local storage to keep the list of people and their asigned
+                            chore active until removed. A color is asigned to each name to keep the list of chores organized.</p>
+                            <button type='button' className='btn btn-outline-primary'><a href='https://christiandenny.com/chore-app'>Check out Chore Time</a></button>
 
                         </div>
                     </div>
@@ -100,9 +104,10 @@ export default function Projects(props) {
                             alt='Story landing page'
                         />
                         <div className='card-body'>
-                            <p> <a href='https://dtc-wsuv.org/cdenny17/story/'>Check out The Story</a></p>
-                            <p>I built the story site with JavaScript using Pixabay's API. Whenever users fill-in portions of the "mad-lib" style story a
+                            <h3>The Story</h3>
+                            <p>I built The Story site with JavaScript using Pixabay's API. Whenever users fillin portions of the "mad-lib" style story, a
                             related picture is displayed from Pixabay.</p>
+                            <button type='button' className='btn btn-outline-primary' ><a href='https://dtc-wsuv.org/cdenny17/story/' >Check out The Story</a></button>
 
                         </div>
                     </div>
@@ -110,6 +115,6 @@ export default function Projects(props) {
 
 
             </div>
-        </div>
+        </div >
     );
 }
